387e643ee73c390bc4495c7a69396bcbae1e7fd1
rbsteffes <45981093+rbsteffes@users.noreply.github.com>
2022-10-04 21:10:21 -0400

    Updates for class

--- server/node_js_server.js
+++ server/node_js_server.js
@@ -6,6 +6,14 @@ const path = require('path');
 const { allowedNodeEnvironmentFlags } = require('process');
 //Set up the router
 app.use(express.static(path.join(__dirname, 'public')));
+var bodyParser = require('body-parser')
+// create application/json parser
+var jsonParser = bodyParser.json();
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+app.use(jsonParser);
+app.use(urlencodedParser);
 
 // Data for server
 var reviews = [
@@ -42,7 +50,6 @@ router.get(
                 function(req, res){
                         res.sendFile(path.join(__dirname, "JavaScriptObject__Reviews.html"));
                 });
-app.use('/', router);
 
 //Answer "get" when we call "api/reviews"
 router.get(
@@ -51,7 +58,112 @@ router.get(
                 let currentIndex= Math.floor(Math.random() * reviews.length);
                 res.send(reviews[currentIndex]);
             }
-)
+);
+
+//Answer "get" when we call "api/reviews/:id"
+router.get(
+    '/api/reviews/:id',
+    function(req,res){
+        
+        let found = reviews.find(function(item){
+                return item.id === parseInt(req.params.id);
+        } );
+
+        console.log(found);
+        if(found)
+            res.status(200).json(found);
+        else res.status(404);
+        
+    }
+);
+
+
+//Answer "delete" when we call "api/reviews/:id"
+router.delete(
+    '/api/reviews/:id',
+    function(req,res){
+        
+        let found = reviews.find(function(item){
+                return item.id === parseInt(req.params.id);
+        } );
+
+        console.log(found);
+        if(found)
+        {  
+            let foundIndex = reviews.indexOf(found);
+            reviews.splice(foundIndex,1);
+        }
+
+        console.log(reviews);
+        res.sendStatus(204);
+        
+        
+    }
+);
+
+
+//CREATE
+//Answer "post" when we call "api/reviews"
+router.post(
+    '/api/reviews',
+    function(req,res){
+        console.log("Handling POST");
+        console.log(req.body);
+        let itemIds = reviews.map(item => item.id);
+        let newReview = {    
+            id: (itemIds.length >0)? Math.max.apply(Math, itemIds)+1: 1,
+            name: req.body.name,
+            job: req.body.job,
+            img: req.body.img,
+            text: req.body.review
+         }
+
+         reviews.push(newReview);
+         res.status(201).json(newReview);
+       
+    }
+);
+
+//CREATE
+//Answer "post" when we call "api/reviews"
+router.post(
+    '/api/reviews',
+    function(req,res){
+        console.log("Handling PUT");
+        console.log(req.body);
+    
+        let newReview = {    
+            id: req.body.id,
+            name: req.body.name,
+            job: req.body.job,
+            img: req.body.img,
+            text: req.body.text
+         }
+
+         let found = reviews.find(function(item){
+            return item.id === parseInt(newReview.id);
+            } );
+ 
+        if(found)
+        {  
+            let foundIndex = reviews.indexOf(found);
+            reviews.splice(foundIndex,1,newReview);
+            res.status(204).json(newReview);
+        }
+        else{
+            res.status(404);
+        }
+        
+       
+    }
+);
+
+
+
+
+app.use('/', router);
+app.use('/api/reviews', router);
+app.use('/api/reviews/:id', router);
 
 let server = app.listen(3000, function(){
     console.log("App server is running on port 3000");
--- server/public/js_objects_review.js
+++ server/public/js_objects_review.js
@@ -1,10 +1,12 @@
 
+var currentReviewID = 1;
 
 function loadReview()
 {
+    
     //Make and AJAX call
     let xhr = new XMLHttpRequest();
-    xhr.open("get","/api/reviews", true);
+    xhr.open("get","/api/reviews/" + currentReviewID, true);
     xhr.send();
     xhr.onreadystatechange = function(){
         if(xhr.readyState == xhr.DONE)
@@ -19,24 +21,63 @@ function loadReview()
             revJob.innerHTML = reviews.job;
             revReview.innerHTML = reviews.text;
             revImage.src = reviews.img;
+            currentReviewID = reviews.id;
         }
          };
+
+         $("#reviewInput").hide();
+}
+
+function newReview(){
+    $("#reviewDisplay").hide();
+    $("#reviewInput").show();
+}
+function saveReview(){
+      //Make and AJAX call
+      let xhr = new XMLHttpRequest();
+      xhr.open("post","/api/reviews/", true);
+
+      let newReview = {
+        name : $("#revName").val(),
+        job : $("#revJob").val(),
+        review : $("#revTextReview").val(),
+      };
+      xhr.setRequestHeader("Content-Type", "application/json");
+      xhr.send(JSON.stringify(newReview));
+      console.log(JSON.stringify(newReview));
+      xhr.onreadystatechange = function(){
+          if(xhr.readyState == xhr.DONE)
+          {
+            var item = JSON.parse(xhr.responseText);
+            currentReviewID = parseInt(item.id);
+            loadReview();
+        }};
+
+    $("#reviewInput").hide();
+    $("#reviewDisplay").show();
+    return false;
+    
+}
+
+function deleteReview(){
+    //Make and AJAX call
+    let xhr = new XMLHttpRequest();
+    xhr.open("delete","/api/reviews/"+ currentReviewID, true);
+    xhr.send();
+    xhr.onreadystatechange = function(){
+        if(xhr.readyState == xhr.DONE)
+        {
+            currentReviewID++;
+            loadReview();
+        }
 }
 
 function nextReview(){
-    currentIndex++;
-    if( currentIndex >= reviews.length)
-        currentIndex=0;
-
     loadReview();
 }
 
-function prevReview(){
-    currentIndex--;
-    if( currentIndex < 0)
-        currentIndex = reviews.length - 1;
-    
-    loadReview();
+function prevReview(){}
+    loadReview("next=false");
 }
 
 loadReview();
